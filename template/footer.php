b54b0a3c30349c4472dca4bcef798779085e2f39
iwamoto-seisuke <af189264@gmail.com>
2024-01-28 16:32:15 +0900

    共通ヘッダーとフッターを作成

---
+++ template/footer.php
@@ -0,0 +1,12 @@
+</body>
+<footer class="bg-[#117f7f] text-white mt-8 py-4">
+  <div class="container mx-auto text-center">
+    <h2 class="text-xl font-bold mb-2">Yahoo!知恵袋の様な掲示板サイト</h2>
+    <p class="mb-2">&copy; 2024 Yahoo!知恵袋の様な掲示板サイト. All rights reserved.</p>
+    <button onclick="topFunction()" id="myBtn" title="Go to top" class="bg-black text-white py-2 px-4 rounded">
+      ページトップへ
+    </button>
+  </div>
+</footer>
+
+</html>
---
+++ template/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" class="scroll-smooth">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $pageTitle; ?></title>
+    <link rel="stylesheet" href="./assets/css/style.css?<?php echo time(); ?>">
+</head>
+
+<body class="bg-gray-100">
+<header class="py-4">
+  <div class="bg-white shadow-md rounded-full py-4 px-8 mx-auto max-w-7xl flex justify-between items-center">
+    <div class="logo">
+      <h1 class="text-black">Yahoo!知恵袋の様な掲示板サイト</h1>
+    </div>
+    <nav class="">
+      <ul class="flex gap-3">
+        <li class="">
+          <a class="bg-black hover:bg-[#404040] duration-300 text-white block py-2 px-4 rounded mr-2" href="">ログイン</a>
+        </li>
+        <li class="">
+          <a class="bg-[#fc7f11] hover:bg-[#fd9f4d] duration-300 text-white block py-2 px-4 rounded" href="../users/add/">新規登録</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</header>
