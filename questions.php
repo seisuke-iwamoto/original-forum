4121fefdde42b644439afbbd57866bcd0c8a68c6
iwamoto-seisuke <af189264@gmail.com>
2024-01-28 16:32:42 +0900

    質問一覧画面を修正

---
+++ questions.php
@@ -0,0 +1,19 @@
+<?php
+$pageTitle = 'Yahoo!知恵袋の様な掲示板サイト';
+require_once('./template/header.php');
+?>
+
+<!-- 投稿カード -->
+<div class="container mx-auto my-8">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <!-- カードの例 -->
+        <div class="bg-white rounded-lg shadow-lg p-4">
+            <h2 class="font-bold text-xl mb-2">投稿タイトル</h2>
+            <p>ここに投稿の内容が入ります。これは例です。</p>
+        </div>
+        <!-- 繰り返し -->
+    </div>
+</div>
+<?php
+require_once('./template/footer.php');
+?>
