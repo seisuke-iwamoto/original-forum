5bd18a4e5bb29cd46b4ca2f6d9ba4d6884237395
iwamoto-seisuke <af189264@gmail.com>
2024-01-29 12:34:06 +0900

    ディレクトリ構成変更に伴いパス修正

--- template/header.php
+++ template/header.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $pageTitle; ?></title>
-    <link rel="stylesheet" href="./assets/css/style.css?<?php echo time(); ?>">
+    <link rel="stylesheet" href="../assets/css/style.css?<?php echo time(); ?>">
+    <script defer src="../assets/js/script.js"></script>
 </head>
 
 <body class="bg-gray-100">
