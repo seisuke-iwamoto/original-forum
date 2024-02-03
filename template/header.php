01fe605c9c931e8438cd2382c58c317d6fd4ac11
iwamoto-seisuke <af189264@gmail.com>
2024-02-03 15:30:14 +0900

    CSS更新

--- template/header.php
+++ template/header.php
@@ -1,3 +1,18 @@
+<?php
+session_start();
+
+$username = $_SESSION['name'];
+if (isset($_SESSION['id'])) { //ログインしているとき
+  $msg = 'こんにちは' . htmlspecialchars($username, \ENT_QUOTES, 'UTF-8') . 'さん';
+  $link = '<a href="logout.php">ログアウト</a>';
+} else { //ログインしていない時
+  $msg = 'ログインしていません';
+  $link = '<a href="login.php">ログイン</a>';
+}
+?>
+<h1><?php echo $msg; ?></h1>
+<?php echo $link; ?>
+
 <!DOCTYPE html>
 <html lang="en" class="scroll-smooth">
 
@@ -17,19 +32,30 @@
       </div>
       <nav>
         <ul class="flex gap-3">
-          <li>
-            <a class="bg-black hover:bg-[#404040] duration-300 text-white block py-2 px-4 rounded" href="">
-              ログイン
-            </a>
-          </li>
-          <li>
-            <a class="bg-[#fc7f11] hover:bg-[#fd9f4d] duration-300 text-white block py-2 px-4 rounded" href="../users/add/">新規登録
-            </a>
-          </li>
-          <li>
-            <a class="bg-gray-200 hover:bg-gray-400 duration-300 text-black block py-2 px-4 rounded" href="../users/add/">ログアウト
-            </a>
-          </li>
+          <?php if (isset($_SESSION['id'])) : ?>
+            <li>
+              <a class="hover:opacity-70 duration-300 text-black flex flex-col gap-2 items-center" href="../users/add/">
+                <div class="max-w-20">
+                  <img src="../assets/img/mypage-icon.png" alt="" width="80" height="80" class="w-full h-full object-cover object-center">
+                </div>
+                <span>マイページ</span>
+              </a>
+            </li>
+            <li>
+              <a class="bg-gray-200 hover:bg-gray-400 duration-300 text-black block py-2 px-4 rounded" href="../users/add/">ログアウト
+              </a>
+            </li>
+          <?php else : ?>
+            <li>
+              <a class="bg-black hover:bg-[#404040] duration-300 text-white block py-2 px-4 rounded" href="">
+                ログイン
+              </a>
+            </li>
+            <li>
+              <a class="bg-[#fc7f11] hover:bg-[#fd9f4d] duration-300 text-white block py-2 px-4 rounded" href="../users/add/">新規登録
+              </a>
+            </li>
+          <?php endif; ?>
         </ul>
       </nav>
     </div>
