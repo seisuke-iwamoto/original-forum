59e51cac11ebdde85e03e52959ebc05d8e7d8e48
iwamoto-seisuke <af189264@gmail.com>
2024-02-04 15:05:59 +0900

    ヘッダーのレイアウト調整とボタンのリンク指定

--- template/header.php
+++ template/header.php
@@ -3,11 +3,11 @@ session_start();
 
 $username = $_SESSION['name'];
 if (isset($_SESSION['id'])) { //ログインしているとき
-  $msg = 'こんにちは' . htmlspecialchars($username, \ENT_QUOTES, 'UTF-8') . 'さん';
-  $link = '<a href="logout.php">ログアウト</a>';
+  // $msg = 'こんにちは' . htmlspecialchars($username, \ENT_QUOTES, 'UTF-8') . 'さん';
+  // $link = '<a href="logout.php">ログアウト</a>';
 } else { //ログインしていない時
-  $msg = 'ログインしていません';
-  $link = '<a href="login.php">ログイン</a>';
+  // $msg = 'ログインしていません';
+  // $link = '<a href="login.php">ログイン</a>';
 }
 ?>
 <h1><?php echo $msg; ?></h1>
@@ -31,25 +31,25 @@ if (isset($_SESSION['id'])) { //ログインしているとき
         <h1 class="text-black">Yahoo!知恵袋の様な掲示板サイト</h1>
       </div>
       <nav>
-        <ul class="flex gap-3">
+        <ul class="flex gap-4 items-center">
           <?php if (isset($_SESSION['id'])) : ?>
             <li>
-              <a class="hover:opacity-70 duration-300 text-black flex flex-col gap-2 items-center" href="../users/add/">
+              <a class="hover:underline  text-black flex flex-col gap-2 items-center" href="#">
                 マイページ
               </a>
             </li>
             <li>
-              <a class="bg-gray-200 hover:bg-gray-400 duration-300 text-black block py-2 px-4 rounded" href="../users/add/">ログアウト
+              <a class="bg-gray-200 hover:bg-gray-400 duration-300 text-black block py-2 px-4 rounded" href="../logout/">ログアウト
               </a>
             </li>
           <?php else : ?>
             <li>
-              <a class="bg-black hover:bg-[#404040] duration-300 text-white block py-2 px-4 rounded" href="">
+              <a class="bg-black hover:bg-[#404040] duration-300 text-white block py-2 px-4 rounded" href="../login/">
                 ログイン
               </a>
             </li>
             <li>
-              <a class="bg-[#fc7f11] hover:bg-[#fd9f4d] duration-300 text-white block py-2 px-4 rounded" href="../users/add/">新規登録
+              <a class="bg-[#fc7f11] hover:bg-[#fd9f4d] duration-300 text-white block py-2 px-4 rounded" href="../users/">新規登録
               </a>
             </li>
           <?php endif; ?>
