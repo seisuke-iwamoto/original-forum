c99cf05ad3c76363f0454362b69cc47fd22e5474
iwamoto-seisuke <af189264@gmail.com>
2024-02-17 16:10:03 +0900

    質問一覧画面 質問の情報をDBから取得、表示まで実装

--- questions/index.php
+++ questions/index.php
@@ -1,21 +1,35 @@
 <?php
+ob_start();
 // ディレクトリ階層に合わせてファイルパスを定義
 $root_pass = '../';
+require($root_pass . 'dbconect.php');
+session_start();
+
+// 質問の投稿情報を取得
+$posts_query = $db->query('SELECT u.nickname, q.* FROM users u, questions q WHERE u.id=q.user_id ORDER BY q.create_date DESC');
+$posts = $posts_query->fetchAll(); //SQL文の結果を全て取り出し
+?>
+
+<?php
 // ヘッダー
 $page_title = 'Yahoo!知恵袋の様な掲示板サイト';
 require_once($root_pass . 'template/header.php');
 ?>
 
-<!-- 投稿カード -->
+<!-- 質問一覧 -->
 <div class="container mx-auto my-8">
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <!-- カードの例 -->
-        <div class="bg-white rounded-lg shadow-lg p-4">
-            <h2 class="font-bold text-xl mb-2">投稿タイトル</h2>
-            <p>ここに投稿の内容が入ります。これは例です。</p>
-        </div>
-        <!-- 繰り返し -->
-    </div>
+    <ul class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <?php foreach ($posts as $post) : ?>
+            <li class="bg-white rounded-lg shadow-lg p-4">
+                <h2 class="font-bold text-xl mb-2">
+                    <?php echo htmlspecialchars($post['nickname']); ?>
+                </h2>
+                <p>
+                    <?php echo mb_strimwidth(htmlspecialchars($post['body']), 0, 300, '...'); ?>
+                </p>
+            </li>
+        <?php endforeach; ?>
+        </う>
 </div>
 
 <?php
