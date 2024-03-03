4507088eae8119b5e4aa5d2e336d318cb331da85
iwamoto-seisuke <af189264@gmail.com>
2024-03-03 11:04:24 +0900

    回答削除用の処理を実装

---
+++ answers/delete/index.php
@@ -0,0 +1,25 @@
+<?php
+ob_start();
+// ディレクトリ階層に合わせてファイルパスを定義
+$root_pass = '../../';
+require($root_pass . 'dbconect.php');
+session_start();
+
+if (isset($_SESSION['id'])) {
+  $id = $_REQUEST['id'];
+
+  // 回答を検査する
+  $answers = $db->prepare('SELECT * FROM answers WHERE id=?');
+  $answers->execute(array($id));
+  $answer = $answers->fetch();
+
+  if ($answer['user_id'] == $_SESSION['id']) {
+    // 削除する
+    $del = $db->prepare('UPDATE answers SET update_date=NOW(), delete_flag=true WHERE id=?');
+    $del->execute(array($id));
+  }
+}
+
+header('Location: ../../questions/view.php?id=' . $_SESSION['question_id']);
+exit();
+?>
