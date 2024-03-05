a9025ad82d552e690426f35031b4e9802a8ce228
iwamoto-seisuke <af189264@gmail.com>
2024-03-05 12:00:51 +0900

    フッターのページトップがいらないので削除

--- template/footer.php
+++ template/footer.php
@@ -1,10 +1,7 @@
-<footer class="bg-[#117f7f] text-white mt-8 py-4">
+<footer class="bg-blue-500 text-white mt-8 py-4">
   <div class="container mx-auto text-center">
     <h2 class="text-xl font-bold mb-2">Yahoo!知恵袋の様な掲示板サイト</h2>
     <p class="mb-2">&copy; 2024 Yahoo!知恵袋の様な掲示板サイト. All rights reserved.</p>
-    <button onclick="topFunction()" id="myBtn" title="Go to top" class="bg-black text-white py-2 px-4 rounded">
-      ページトップへ
-    </button>
   </div>
 </footer>
 </div>
